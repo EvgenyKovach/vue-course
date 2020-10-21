799f1371ac1002c308170a416910f6ce17cf9ee6
kovach <zheka.ae@mail.ru>
2020-10-21 19:08:21 +0500

    next 29

--- notes/src/App.vue
+++ notes/src/App.vue
@@ -4,8 +4,33 @@
     <div class="wrapper-content">
       <section>
         <div class="container">
-          <h1>Hello</h1>
-          <img alt="Vue logo" src="./assets/logo.png">
+          
+        <h1> {{ title }} </h1>
+
+          <message v-if='message' :message='message'/>
+
+          <div class="new-note">
+            <input v-model='note.title' type="text">
+            <textarea v-model='note.descr' ></textarea>
+            <button @click="addNote">Push</button>
+          </div>
+
+          <div class="notes">
+
+            <div class="note" v-for="(note, index) in notes" :key='index'>
+              
+              <div class="note-header">
+                <h2> {{ note.title }}  </h2>
+              </div>
+              <div class="note-body">
+                <p>{{ note.descr  }}</p>
+                <p>{{ note.date }}</p>
+              </div>
+
+            </div>
+
+          </div>
+
         </div>
       </section>
     </div>
@@ -14,7 +39,57 @@
 </template>
 
 <script>
-</script>
+import message from '@/components/Message.vue'
 
+export default {
+  components: {
+    message
+  },
+  data () {
+      return {
+        title: 'Notes App',
+        message: null,
+        note: {
+          title: '',
+          descr: ''
+        },
+        notes: [
+          {
+            title: 'first note',
+            descr: 'desr for first note',
+            date: new Date(Date.now()).toLocaleString()
+          },
+          {
+            title: 'second note',
+            descr: 'desr for second note',
+            date: new Date(Date.now()).toLocaleString()
+          },
+          {
+            title: 'third note',
+            descr: 'desr for third note',
+            date: new Date(Date.now()).toLocaleString()
+          }
+        ]
+      }
+    },
+    methods: {
+      addNote() {
+        // console.log(this.note)
+        let {title, descr} = this.note
+
+        if (title === '') return this.message = 'need title'
+
+        this.notes.push({
+          title: title,
+          descr: descr,
+          date: new Date(Date.now()).toLocaleString()
+        })
+        this.note.title =''
+        this.note.descr =''
+        this.message = null
+      }
+  }
+}
+</script>
 <style>
 </style>
---
+++ notes/src/components/Message.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="message">
+        <p>
+            Message is -- {{  message  }}
+        </p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        message: {
+            type: String,
+            required: true,
+        }
+    }
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+    .message {
+        text-align: center;
+    }
+
+    p {
+        color: red
+    }
+
+</style>
