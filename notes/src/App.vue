eb02ac98b762b21e620a583dbb59c61d3fcfeb58
kovach <zheka.ae@mail.ru>
2020-11-05 18:56:27 +0500

    next 34/ DZ

--- notes/src/App.vue
+++ notes/src/App.vue
@@ -10,15 +10,21 @@
 
           <newNote :note='note' @addNote='addNote' />
 
-          <div class="note-title">
-            <h1> {{ title }} </h1>
+          <div class="note-title" style="margin: 36px 0">
+
+            <h1> {{ title }} </h1> 
+
+            <search  :value='search' 
+                      placeholder='find ur note' 
+                      @search='search = $event'/>
+
             <div class="icons">
               <svg :class="{ active: grid }" @click="grid = true" xmlns="http://www.w3.org/2000/svg" style='cursor: pointer' width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg>
               <svg :class="{ active: !grid }" @click="grid = false"  xmlns="http://www.w3.org/2000/svg" style='cursor: pointer' width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="8" y1="6" x2="21" y2="6"></line><line x1="8" y1="12" x2="21" y2="12"></line><line x1="8" y1="18" x2="21" y2="18"></line><line x1="3" y1="6" x2="3" y2="6"></line><line x1="3" y1="12" x2="3" y2="12"></line><line x1="3" y1="18" x2="3" y2="18"></line></svg>
             </div>
           </div>
 
-          <notes :notes='notes' :grid="grid" @remove='removeNote' />
+          <notes :notes='notesFilter' :grid="grid" @remove='removeNote' />
 
         </div>
       </section>
@@ -31,16 +37,19 @@
 import message from '@/components/Message.vue'
 import newNote from '@/components/NewNote.vue'
 import notes from '@/components/Notes.vue'
+import search from '@/components/Search.vue'
 
 export default {
   components: {
     message,
     newNote,
-    notes
+    notes,
+    search
   },
   data () {
       return {
         title: 'Notes App',
+        search: '',
         message: null,
         grid: true,
         note: {
@@ -64,6 +73,24 @@ export default {
             date: new Date(Date.now()).toLocaleString()
           }
         ]
+      }
+    },
+    computed: {
+      notesFilter () {
+        let array = this.notes,
+            search = this.search
+        
+        if (!search) return array
+
+        search = search.trim().toLowerCase()
+
+        array = array.filter(function (item) {
+          if (item.title.toLowerCase().indexOf(search) !== -1) {
+            return item
+          }
+        })
+
+        return array
       }
     },
     methods: {
